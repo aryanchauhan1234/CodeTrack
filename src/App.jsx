8b673d69975c5efb940fed5edda5f767e8ca0957
aryanchauhan1234 <chauhanaryan2453@gmail.com>
2025-05-10 14:00:55 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './Components/Navbar';
+
+
+function App() {
+  return (
+    <div>
+        <Navbar/>
+    </div>
+  );
+}
+
+export default App;
+
+
